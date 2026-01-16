2dab26cd851ddaccf2dfd12012ce50027e945a5e
Jose Sagastume <jose.sagast@gmail.com>
2026-01-16 16:32:04 -0700

    extended front end

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,77 @@
-import { useContext } from 'react';
-import { Routes, Route } from 'react-router';
+import { useContext, useState, useEffect } from 'react';
+import { Routes, Route, useNavigate } from 'react-router';
 
 import NavBar from './components/NavBar/NavBar';
 import SignUpForm from './components/SignUpForm/SignUpForm';
 import SignInForm from './components/SignInForm/SignInForm';
 import Landing from './components/Landing/Landing';
 import Dashboard from './components/Dashboard/Dashboard';
+import HootList from './components/HootList/HootList';
+import HootDetails from './components/HootDetails/HootDetails';
+import HootForm from './components/HootForm/HootForm';
+
 
 import { UserContext } from './contexts/UserContext';
 
+import * as hootService from './services/hootService'
+
 const App = () => {
   const { user } = useContext(UserContext);
-  
+
+  const [hoots, setHoots] = useState([]);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const fetchAllHoots = async () => {
+      const hootsData = await hootService.index();
+      setHoots(hootsData);
+    };
+    if (user) fetchAllHoots();
+  }, [user]);
+
+  const handleAddHoot = async (hootFormData) => {
+    const newHoot = await hootService.create(hootFormData);
+    setHoots([newHoot, ...hoots]);
+    navigate('/hoots');
+  };
+
+  const handleDeleteHoot = async (hootId) => {
+    const deletedHoot = await hootService.deleteHoot(hootId);
+    setHoots(hoots.filter((hoot) => hoot._id !== hootId));
+    navigate('/hoots');
+  };
+
+  const handleUpdateHoot = async (hootId, hootFormData) => {
+    const updatedHoot = await hootService.update(hootId, hootFormData);
+    setHoots(hoots.map((hoot) => (hootId === hoot._id ? updatedHoot : hoot)));
+    navigate(`/hoots/${hootId}`);
+  };
+
   return (
-    <>
-      <NavBar/>
-      <Routes>
-        <Route path='/' element={user ? <Dashboard /> : <Landing />} />
-        <Route path='/sign-up' element={<SignUpForm />} />
-        <Route path='/sign-in' element={<SignInForm />} />
-      </Routes>
-    </>
-  );
+      <>
+        <NavBar/>
+        <Routes>
+          <Route path='/' element={user ? <Dashboard /> : <Landing />} />
+          {/* conditionals to show content to authorized */}
+          {user ? (
+            <>
+              {/* Protected routes (available only to signed-in users) */}
+              <Route path='/hoots' element={<HootList hoots={hoots} />} />
+              <Route path='/hoots/:hootId'element={<HootDetails />}/>
+              <Route path='/hoots/new' element={<HootForm handleAddHoot={handleAddHoot} />} />
+              <Route path='/hoots/:hootId'element={<HootDetails handleDeleteHoot={handleDeleteHoot}/>}/>
+              <Route path='/hoots/:hotId/edit' element={<HootForm handleUpdateHoot={handleUpdateHoot} />}/>
+            </>
+          ) : (
+            <>
+              {/* Non-user routes (available only to guests) */}
+              <Route path='/sign-up' element={<SignUpForm />} />
+              <Route path='/sign-in' element={<SignInForm />} />
+            </>
+          )}
+        </Routes>
+      </>
+    );
 };
 
 export default App;
